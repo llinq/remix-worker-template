b27351199427958fe45bc1bae32d73904f779062
Joao Costa <joaopedrocosta.contato@gmail.com>
2024-06-13 19:40:29 -0300

    Change layouts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,26 +1,26 @@
-import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
-import { json, useLoaderData } from '@remix-run/react';
-import { Markdown } from '~/components';
-import { getFileContentWithCache } from '~/services/github.server';
-import { parse } from '~/services/markdoc.server';
+// import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
+// import { json, useLoaderData } from '@remix-run/react';
+// import { Markdown } from '~/components';
+// import { getFileContentWithCache } from '~/services/github.server';
+// import { parse } from '~/services/markdoc.server';
 
-export async function loader({ context }: LoaderFunctionArgs) {
-	const content = await getFileContentWithCache(context, 'README.md');
+// export async function loader({ context }: LoaderFunctionArgs) {
+// 	const content = await getFileContentWithCache(context, 'README.md');
 
-	return json(
-		{
-			content: parse(content),
-		},
-		{
-			headers: {
-				'Cache-Control': 'public, max-age=3600',
-			},
-		},
-	);
-}
+// 	return json(
+// 		{
+// 			content: parse(content),
+// 		},
+// 		{
+// 			headers: {
+// 				'Cache-Control': 'public, max-age=3600',
+// 			},
+// 		},
+// 	);
+// }
 
-export default function Index() {
-	const { content } = useLoaderData<typeof loader>();
+// export default function Index() {
+// 	const { content } = useLoaderData<typeof loader>();
 
-	return <Markdown content={content} />;
-}
+// 	return <Markdown content={content} />;
+// }
