20cc75666e5f109a61d9192f97e3b9a1df770a2a
Joao Costa <joaopedrocosta.contato@gmail.com>
2024-06-13 19:42:55 -0300

    Change routes index

---
+++ app/routes/index.tsx
@@ -0,0 +1,18 @@
+// export async function loader({ context }: LoaderFunctionArgs) {
+// 	const content = await getFileContentWithCache(context, 'README.md');
+
+// 	return json(
+// 		{
+// 			content: parse(content),
+// 		},
+// 		{
+// 			headers: {
+// 				'Cache-Control': 'public, max-age=3600',
+// 			},
+// 		},
+// 	);
+// }
+
+export default function Index() {
+  return <h1> Routes | Index</h1>;
+}
