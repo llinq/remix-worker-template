7427265f5b9048566d433e4dd07b6dd01e63c784
Joao Costa <joaopedrocosta.contato@gmail.com>
2024-06-14 12:13:38 -0300

    change entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,26 +1,54 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
-import { RemixServer } from "@remix-run/react";
+// entry.server.tsx
+import { renderToReadableStream, renderToString } from 'react-dom/server'
+import { CacheProvider } from '@emotion/react'
+import createEmotionServer from '@emotion/server/create-instance'
+import { RemixServer } from '@remix-run/react'
+import type { EntryContext } from '@remix-run/cloudflare' // Depends on the runtime you choose
 import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+
+import { ServerStyleContext } from './context'
+import createEmotionCache from './createEmotionCache'
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadContext: AppLoadContext
 ) {
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  // const html = renderToString(
+  //   <ServerStyleContext.Provider value={null}>
+  //     <CacheProvider value={cache}>
+  //       <RemixServer context={remixContext} url={request.url} />
+  //     </CacheProvider>
+  //   </ServerStyleContext.Provider>,
+  // );
+
+  // const chunks = extractCriticalToChunks(html);
+
+  // const markup = renderToString(
+  //   <ServerStyleContext.Provider value={chunks.styles}>
+  //     <CacheProvider value={cache}>
+  //       <RemixServer context={remixContext} url={request.url} />
+  //     </CacheProvider>
+  //   </ServerStyleContext.Provider>,
+  // );
+
+  // responseHeaders.set('Content-Type', 'text/html');
+
+  // return new Response(`<!DOCTYPE html>${markup}`, {
+  //   status: responseStatusCode,
+  //   headers: responseHeaders,
+  // });
+
   const body = await renderToReadableStream(
-    <RemixServer context={remixContext} url={request.url} />,
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>,
     {
       signal: request.signal,
       onError(error: unknown) {
@@ -40,4 +68,5 @@ export default async function handleRequest(
     headers: responseHeaders,
     status: responseStatusCode,
   });
+
 }
